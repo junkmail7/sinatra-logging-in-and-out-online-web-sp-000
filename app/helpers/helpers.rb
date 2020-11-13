76f354c203c576358f822355249ecf1ce5b08d43
William Pannell <lpann5@gmail.com>
2020-11-13 23:13:57 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def current_user(session_hash)
+  def self.current_user(session_hash)
     @user = User.find(session_hash[:user_id])
     return @user
   end
-  def is_logged_in?(session_hash)
+  def self.is_logged_in?(session_hash)
     if session_hash[:user_id]
       return true
     else
